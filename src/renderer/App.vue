9aaf80edbbb42bf09827c1fec0ac57f9ed4c5c03
Patryk Gała | Atrem <Patryk.Gala@ATREM.PL>
2019-01-16 20:51:27 +0100

    Init

---
+++ src/renderer/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      name: 'Obligassdaddasdfsdftory Bear'
+    }
+  }
+}
+</script>
